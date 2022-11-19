d0752865593d6d916dc4673afb68475c82bc84e0
reddragius <47450480+reddragius@users.noreply.github.com>
2022-11-19 14:53:09 +0100

    Admin CSS

--- admin.php
+++ admin.php
@@ -40,7 +40,7 @@
 	if (array_key_exists("loggedInUser", $_SESSION)) 
 	{	
 		$instanceCurrentPage = null;
-		
+
 		// zpracovani vyberu aktualni stranky
 		if (array_key_exists("page", $_GET)) 
 		{
@@ -66,87 +66,129 @@
 	<meta name="description" content="Page description.">
 	<meta name="robots" content="index">
 	
-	<link rel="stylesheet" href="css/main.min.css">
-	<link rel="stylesheet" href="css/all.min.css">
+	<link rel="stylesheet" href="css/admin.css">
+	<link rel="stylesheet" href="css/all.min.css"><!-- css fontawesome  -->
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link rel="shortcut icon" href="./favicon.png" type="image/x-icon">
 	<link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	
 	
 	<title>Admin sekce</title>
 </head>
 <body>
-	<?php
-		if (array_key_exists("loggedInUser", $_SESSION) == false) 
-		{
-			// odhlaseny uzivatel
-			?>
-			<form method="post">
-				<label>Přihl. jméno: <input type="text" name="user"></label><br>
-				<label>Heslo: <input type="password" name="password"></label><br>
-				<button name="login-button">Login</button>
-			</form> 
-			<?php 
-				echo $error;
-		} 
-		else 
-		{
-			//sekce pro prihlasene uzivatele
-			echo "<h2>Přihlášen uživatel: {$_SESSION["loggedInUser"]}</h2>";
-			echo "<form method='post'>
-					<button name='logout-button'>Logout</button>
-				</form>";
-			echo "<br>";
-			echo "<ul>";
-				foreach ($pageList as $pageID => $page) 
-				{
-					echo "<li>
-							<a href='?page=$pageID'>$pageID</a> | 
-							<a href='$pageID' target='_blank'><i class='fa-solid fa-eye'></i></a>
-						</li>";
-				};
-			echo "</ul>";
-			
-			// editacni formular, zobrazit pokud je nejaka stranka vybrana k editaci
-			if (isset($instanceCurrentPage))
+	<div class="admin-body">
+		<?php
+			if (array_key_exists("loggedInUser", $_SESSION) == false) 
 			{
-				echo "<h2>Editace stránky: $instanceCurrentPage->pageID</h2>";
+				// odhlaseny uzivatel
 				?>
-				<form method="post">
-					<textarea id="myTinymce" name="content" cols="80" rows="15" ><?php
-						echo htmlspecialchars($instanceCurrentPage->getContent());
-					?></textarea>
-					<br>
-					<button name="save-button">Uložit</button>
-				</form>
-				<script src="vendor/tinymce/tinymce/tinymce.min.js"></script>
-				<script>
-					tinymce.init({
-						selector: '#myTinymce',
-						language: 'cs',
-						language_url: '<?php echo dirname($_SERVER["PHP_SELF"]); ?>/vendor/tweeb/tinymce-i18n/langs/cs.js',
-						height: '50vh', // vyska okna editoru
-						entity_encoding: "raw", // fix kodovani
-						verify_html: false, // fix fontawesome icon - empty space
-						content_css: [
-							"css/main.min.css",
-							"css/all.min.css", 
-							"https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Open+Sans:wght@300;400;700&display=swap"
-						], //load my css, fontawesome css, google font css
-						plugins: 'advlist anchor autolink charmap code colorpicker contextmenu directionality emoticons fullscreen hr image imagetools insertdatetime link lists nonbreaking noneditable pagebreak paste preview print save searchreplace tabfocus table textcolor textpattern visualchars',
-						toolbar1: "insertfile undo redo | styleselect | fontselect | fontsizeselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | forecolor backcolor",
-                    	toolbar2: "link unlink anchor | fontawesome | image media | responsivefilemanager | preview code",
-						// filemanager - adds a new button for upload. Its needs directories: ./upload/source & ./upload/thumbs
-						external_plugins: {
-							'responsivefilemanager': '<?php echo dirname($_SERVER['PHP_SELF']); ?>/vendor/primakurzy/responsivefilemanager/tinymce/plugins/responsivefilemanager/plugin.min.js',
-						},
-						external_filemanager_path: "<?php echo dirname($_SERVER['PHP_SELF']); ?>/vendor/primakurzy/responsivefilemanager/filemanager/",
-						filemanager_title: "Správce souborů",
-					});
-				</script>
+				<main class="form-signin">
+					<form method="post">
+						<h1 class="h3 mb-3 fw-normal">Přihlašte se prosím</h1>
+
+						<?php if ($error != "") { ?>
+							<div class="alert alert-danger" role="alert">
+								<?php echo $error; ?>
+							</div>
+						<?php } ?>
+
+						<div class="form-floating">
+							<input name="user" type="text" class="form-control" id="floatingInput" placeholder="login">
+							<label for="floatingInput">Přihlašovací jméno</label>
+						</div>
+						<div class="form-floating">
+							<input name="password" type="password" class="form-control" id="floatingPassword" placeholder="heslo">
+							<label for="floatingPassword">Heslo</label>
+						</div>
+
+						<button name="login-button" class="w-100 btn btn-lg btn-primary" type="submit">Přihlásit</button>
+					</form>
+            	</main>
+				<?php 
+			} 
+			else 
+			{
+				//sekce pro prihlasene uzivatele
+				echo "<main class='admin'>";
+
+				?>
+				<div class="container">
+					<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+						<div>Přihlášený uživatel: <?php echo $_SESSION["loggedInUser"]; ?></div>
+	
+						<div class="col-md-3 text-end">
+							<form method='post'>
+								<button name='logout-button' class="btn btn-outline-primary me-2">Odhlásit</button>
+							</form>
+						</div>
+					</header>
+				</div>
+	
 				<?php
+				// list pages to edit
+				echo "<ul id='pages' class='list-group'>";
+					foreach ($pageList as $pageID => $page) 
+					{
+						$active = '';
+						$buttonClass = '';
+						if ($page == $instanceCurrentPage)
+						{
+							$active = 'active';
+							$buttonClass = 'btn-secondary';
+						}
+						echo "<li class='list-group-item $active'>
+								<a href='?page=$pageID' class='btn $buttonClass'><i class='fa-solid fa-pen-to-square'></i></a>
+								<a href='$pageID' class='btn $buttonClass' target='_blank'><i class='fa-solid fa-eye'></i></a>
+								<span>$pageID</span>
+							</li>";
+					};
+				echo "</ul>";
+				
+				// editacni formular, zobrazit pokud je nejaka stranka vybrana k editaci
+				if (isset($instanceCurrentPage))
+				{
+					echo "<h2>Editace stránky: $instanceCurrentPage->pageID</h2>";
+					?>
+					<form method="post">
+						<textarea id="myTinymce" name="content" cols="80" rows="15" ><?php
+							echo htmlspecialchars($instanceCurrentPage->getContent());
+						?></textarea>
+						<br>
+						<button name="save-button" class="btn btn-primary">Uložit</button>
+					</form>
+					<script src="vendor/tinymce/tinymce/tinymce.min.js"></script>
+					<script>
+						tinymce.init({
+							selector: '#myTinymce',
+							language: 'cs',
+							language_url: '<?php echo dirname($_SERVER["PHP_SELF"]); ?>/vendor/tweeb/tinymce-i18n/langs/cs.js',
+							height: '50vh', // vyska okna editoru
+							entity_encoding: "raw", // fix kodovani
+							verify_html: false, // fix fontawesome icon - empty space
+							content_css: [
+								"css/main.min.css",
+								"css/all.min.css", 
+								"https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Open+Sans:wght@300;400;700&display=swap"
+							], //load my css, fontawesome css, google font css
+							plugins: 'advlist anchor autolink charmap code colorpicker contextmenu directionality emoticons fullscreen hr image imagetools insertdatetime link lists nonbreaking noneditable pagebreak paste preview print save searchreplace tabfocus table textcolor textpattern visualchars',
+							toolbar1: "insertfile undo redo | styleselect | fontselect | fontsizeselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | forecolor backcolor",
+							toolbar2: "link unlink anchor | fontawesome | image media | responsivefilemanager | preview code",
+							// filemanager - adds a new button for upload. Its needs directories: ./upload/source & ./upload/thumbs
+							external_plugins: {
+								'responsivefilemanager': '<?php echo dirname($_SERVER['PHP_SELF']); ?>/vendor/primakurzy/responsivefilemanager/tinymce/plugins/responsivefilemanager/plugin.min.js',
+							},
+							external_filemanager_path: "<?php echo dirname($_SERVER['PHP_SELF']); ?>/vendor/primakurzy/responsivefilemanager/filemanager/",
+							filemanager_title: "Správce souborů",
+						});
+					</script>
+					<?php
+				}
+				echo "</main>";
 			}
-		}
-	?>
+		?>
+	</div>
 </body>
 </html>
